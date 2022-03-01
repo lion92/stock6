6c43a778f81bbe9d1aa27d1cac83708fd494a6fb
kriss <kriss@ideta.io>
2022-03-01 05:53:13 +0100

    service personne

---
+++ src/app/interface/Personne.ts
@@ -0,0 +1,12 @@
+export default interface Peronne {
+  idPersonne: number;
+  nom: string;
+  prenom: string;
+  age:number;
+  ville:string;
+  numero:string;
+  adresse:string;
+  codePostale:string;
+  email:string;
+  dateAjout:string;
+}
